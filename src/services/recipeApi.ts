618fe2a819f384a84df41d1df2acb63ffe23792f
enbarSoco <enbar2232@gmail.com>
2022-08-27 15:44:30 +0300

    created recipeApi

---
+++ src/services/recipeApi.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const Application_ID = "ae07fe6a";
+const Application_Keys = "c37ff75b41d900ee430f825bfd5ca535";
+
+export const recipeApi = createApi({
+  reducerPath: "recipeApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.edamam.com/" }),
+  endpoints: (builder) => ({
+    getRecipes: builder.mutation({
+      query: ({ query, health }) => {
+        return {
+          url: `search?q=${query}_id=${Application_ID}&app_key=${Application_Keys}&&health="${health}`,
+        };
+      },
+    }),
+  }),
+});
+
+export const {useGetRecipesMutation} = recipeApi;
